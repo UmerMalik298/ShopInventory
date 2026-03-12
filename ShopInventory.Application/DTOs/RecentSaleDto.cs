c5b4270a3f39e98dd543a30a4c725169e3cf2838
UmerMalik298 <umer23672@gmail.com>
2026-03-12 14:55:46 +0500

    sales

---
+++ ShopInventory.Application/DTOs/RecentSaleDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopInventory.Application.DTOs
+{
+    public class RecentSaleDto
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; } = "";
+        public string? VariantName { get; set; }
+        public int QuantitySold { get; set; }
+        public decimal Revenue { get; set; }
+        public decimal Profit { get; set; }
+        public DateTime SoldAt { get; set; }
+        public string? Notes { get; set; }
+    }
+}
---
+++ ShopInventory.Infrastructure/Services/FileName.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopInventory.Infrastructure.Services
+{
+    internal class FileName
+    {
+        // Add-Migration InitialCreate -Project ShopInventory.Infrastructure -StartupProject ShopInventory.Infrastructure
+
+      //  Update-Database -Project ShopInventory.Infrastructure -StartupProject ShopInventory.Infrastructure
+
+
+    }
+}
