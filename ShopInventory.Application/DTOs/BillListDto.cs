55caebdfb008ec011f97095b98ddfdf129944109
UmerFarooq <umer23672@gmail.com>
2026-03-31 07:02:25 +0500

    Refinement

---
+++ ShopInventory.Application/DTOs/BillListDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopInventory.Domain.Entities.Enums;
+
+namespace ShopInventory.Application.DTOs
+{
+    public class BillListDto
+    {
+        public Guid Id { get; set; }
+        public string BillNo { get; set; } = "";
+        public string? CustomerName { get; set; }
+        public DateTime BilledAt { get; set; }
+        public decimal TotalAmount { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public bool IsDraft { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
